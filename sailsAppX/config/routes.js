91e314afae612712c44c6454729753c58ecb3311
prashantk <pr463475@dal.ca>
2020-07-03 15:14:50 -0300

    job with parts api create, cors config changes

--- sailsAppX/config/routes.js
+++ sailsAppX/config/routes.js
@@ -9,31 +9,29 @@
  */
 
 module.exports.routes = {
-
   /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Make the view located at `views/homepage.ejs` your home page.            *
+   *                                                                          *
+   * (Alternatively, remove this and add an `index.html` file in your         *
+   * `assets` directory)                                                      *
+   *                                                                          *
+   ***************************************************************************/
 
   // '/': { view: 'pages/homepage' },
 
-
   /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * More custom routes here...                                               *
+   * (See https://sailsjs.com/config/routes for examples.)                    *
+   *                                                                          *
+   * If a request to a URL doesn't match any of the routes in this file, it   *
+   * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
+   * not match any of those, it is matched against static assets.             *
+   *                                                                          *
+   ***************************************************************************/
   "/": { view: "pages/jobsMain" },
-  "/homepage":{view:"pages/homepage"},
+  "/homepage": { view: "pages/homepage" },
   "GET /getJobs": "Jobs.getParts",
   "GET /getJobsById/:id/:partId": "Jobs.getPartsById",
   "POST /postJobs": "Jobs.postParts",
@@ -49,7 +47,7 @@ module.exports.routes = {
 
   "GET /getPartorders": "Partorders.getPartorders",
   "POST /postPartorders": "Partorders.postPartorders",
-  
+  "GET /getJobsParts": "Jobs.getJobsParts",
   "GET /getPartordersView": "Partorders.getPartordersView",
   "POST /searchPartOrders": "Partorders.searchPartOrders",
 };
