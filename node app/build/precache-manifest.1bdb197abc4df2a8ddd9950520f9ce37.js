cc039b6aaf86581e3482e0b6c0a76ef0074ee698
raj <rj675423@dal.ca>
2020-07-09 00:45:52 -0300

    node code added

---
+++ node app/build/precache-manifest.1bdb197abc4df2a8ddd9950520f9ce37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01b248390eb74d3c53da47dae802b672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "388512eb49031b497862",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "a7375b3f6937131098d6",
+    "url": "/static/css/main.1b924c76.chunk.css"
+  },
+  {
+    "revision": "388512eb49031b497862",
+    "url": "/static/js/2.c988d876.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c988d876.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7375b3f6937131098d6",
+    "url": "/static/js/main.519921cd.chunk.js"
+  },
+  {
+    "revision": "500f5f429deece5992a4",
+    "url": "/static/js/runtime-main.86ef0fe0.js"
+  }
+]);
